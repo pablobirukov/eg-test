a786d91175d25cd186a5323c36a899bfc49dfcb7
Pavel Birukov <birukov.pavel@gmail.com>
2014-09-11 16:49:45 +0300

    upd pages

--- app_init.js
+++ app_init.js
@@ -1,5 +1,5 @@
 require(['Ctx', 'Bootstrap', 'lib/immutable', 'lib/jquery-2.1.1'], function(Ctx, Bootstrap, Immutable){
-  $.getJSON('/api/getTree.json').done(function(data){
+  $.getJSON('api/getTree.json').done(function(data){
     Ctx.getBinding().set('tree', Immutable.fromJS(data));
     Ctx.React.renderComponent(
       Bootstrap(),
